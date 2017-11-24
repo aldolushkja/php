87932b35c1b46a5956497588a40a97888ad9af2c
Aldo <aldo.lushkja@gmail.com>
2017-11-24 12:04:50 +0100

    made some changes

---
+++ sessions.php
@@ -0,0 +1,20 @@
+<?php
+//Start session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sessions - PHP</title>
+</head>
+<body>
+<?php
+	//Set session variable
+	$_SESSION['username'] = 'devuser';
+	$_SESSION['email'] = 'devuser@gmail.com';
+
+	echo 'Session vars set.';
+?>
+</body>
+</html>
