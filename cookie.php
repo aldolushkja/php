150877134ad9c623d6648e8768eb2ef4331ea23d
Aldo <aldo.lushkja@gmail.com>
2017-11-24 11:44:47 +0100

    added cookie.php

---
+++ cookie.php
@@ -0,0 +1,24 @@
+<?php 
+
+$cookie_name = 'username';
+$cookie_val = 'devuser';
+
+setcookie($cookie_name,$cookie_val,time() * 3600); //set cookie for 1 hour
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cookies - PHP</title>
+</head>
+<body>
+<?php
+	if(!isset($_COOKIE[$cookie_name])){
+		echo 'Cookie - ' .$cookie_name. ' is not set.';
+	} else {
+		echo 'Cookie - ' .$cookie_name. ' is set<br>';
+		echo 'Cookie Value - ' .$cookie_val;
+	}
+?>
+</body>
+</html>
