38bed29c3690b669acdb80374957fc706e929bc7
firetaurus <aldo.lushkja@gmail.com>
2017-11-26 17:40:41 +0100

    Update db.php

--- Php & MySQL/db.php
+++ Php & MySQL/db.php
@@ -1,7 +1,13 @@
 <?php 
 
+//Variables for connecting to your database.
+//These variable values come from your hosting account.
+$hostname = "shareddb1e.hosting.stackcp.net";
+$username = "aldolushkja";
+$dbname = "messageapp-3232b0b7";
+
 //Connect to MySQL
-$connection = mysqli_connect('shareddb1e.hosting.stackcp.net', '3232b0b7','g/AouBkXTGwK','messageapp');
+$connection = mysqli_connect($hostname, $username,'123456db',$dbname);
 
 //Test connection
 
